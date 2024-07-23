fdbcdf3ade23828b4dabe74bd91ebc3d0c975fa7
Ekaterina <ekaterina.saltykova@redeploy.com>
2024-07-23 14:52:32 +0200

    feat: add logging functionality

--- main.go
+++ main.go
@@ -5,37 +5,55 @@ import (
 	"os"
 
 	"github.com/KatrinSalt/notes-service/config"
+	"github.com/KatrinSalt/notes-service/log"
 	"github.com/KatrinSalt/notes-service/server"
 )
 
 func main() {
-	fmt.Println("Starting the note service...")
-	cfg, err := config.New()
-	if err != nil {
-		fmt.Printf("could not load configuration: %s\n", err)
+	logger := setLogger()
+
+	if err := run(logger); err != nil {
+		logger.Error("Server error.", "error", err)
 		os.Exit(1)
 	}
+}
 
-	// DEBUG: Print the configuration struct
-	fmt.Printf("Configuration: %+v\n", cfg)
+func setLogger() *log.Logger {
+	logLevel, set := os.LookupEnv("LOG_LEVEL")
+
+	if set {
+		return log.NewWithSetLevel(logLevel)
+	} else {
+		return log.New()
+	}
+
+}
+func run(log *log.Logger) error {
+	log.Info("Starting the note service.")
+	cfg, err := config.New()
+	if err != nil {
+		return fmt.Errorf("could not load configuration: %w", err)
+	}
+
+	log.Debug("Loaded configuration.", "config", cfg)
 
 	services, err := config.SetupServices(cfg.Services)
 	if err != nil {
-		fmt.Printf("could not setup services: %s\n", err)
-		os.Exit(1)
+		return fmt.Errorf("could not setup services: %w", err)
 	}
 
 	srv, err := server.New(
 		services.Note,
 		server.WithAddress(cfg.Server.Host+":"+cfg.Server.Port),
+		server.WithLogger(log),
 	)
 	if err != nil {
-		fmt.Printf("could not create server: %s\n", err)
-		os.Exit(1)
+		return fmt.Errorf("could not create server: %w", err)
 	}
 
 	if err := srv.Start(); err != nil {
-		fmt.Printf("could not start server: %s\n", err)
-		os.Exit(1)
+		return fmt.Errorf("could not start server: %w", err)
 	}
+	log.Info("Note service stopped.")
+	return nil
 }
